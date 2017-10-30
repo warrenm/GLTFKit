fdc76f6f76ad0714ad11b129fece01db9b3f77c5
Warren Moore <wm@warrenmoore.net>
2017-10-29 18:29:05 -0700

    Added material import to SceneKit integration  - Standard material workflow is almost completely supported  - Need to add customizable texture wrap modes based on glTF texture properties

--- Framework/GLTFSCN/Source/SCNAsset+GLTF.m
+++ Framework/GLTFSCN/Source/SCNAsset+GLTF.m
@@ -18,6 +18,15 @@
 
 #import "SCNAsset+GLTF.h"
 
+
+typedef NS_ENUM(NSInteger, GLTFImageChannel) {
+    GLTFImageChannelRed,
+    GLTFImageChannelGreen,
+    GLTFImageChannelBlue,
+    GLTFImageChannelAlpha,
+    GLTFImageChannelAll = 255
+};
+
 static SCNGeometryPrimitiveType GLTFSCNGeometryPrimitiveTypeForPrimitiveType(GLTFPrimitiveType primitiveType) {
     switch (primitiveType) {
         case GLTFPrimitiveTypePoints:
@@ -38,6 +47,7 @@ static SCNGeometryPrimitiveType GLTFSCNGeometryPrimitiveTypeForPrimitiveType(GLT
 
 @property (nonatomic, strong) GLTFScene *scene;
 @property (nonatomic, copy) NSDictionary<id<NSCopying>, id> *options;
+@property (nonatomic, strong) NSMutableDictionary<NSString *, id> *cgImagesForImagesAndChannels;
 
 - (instancetype)initWithGLTFScene:(GLTFScene *)scene options:(NSDictionary<id<NSCopying>, id> *)options;
 
@@ -51,6 +61,7 @@ static SCNGeometryPrimitiveType GLTFSCNGeometryPrimitiveTypeForPrimitiveType(GLT
     if ((self = [super init])) {
         _scene = scene;
         _options = options;
+        _cgImagesForImagesAndChannels = [NSMutableDictionary dictionary];
     }
     return self;
 }
@@ -65,8 +76,8 @@ static SCNGeometryPrimitiveType GLTFSCNGeometryPrimitiveTypeForPrimitiveType(GLT
     return scnScene;
 }
 
-- (void)recursiveAddNode:(GLTFNode *)node toSCNNode:(SCNNode *)scnNode {
-    SCNNode *childRoot = [SCNNode node];
+- (void)recursiveAddNode:(GLTFNode *)node toSCNNode:(SCNNode *)parentNode {
+    SCNNode *scnNode = [SCNNode node];
     
     if (node.camera != nil) {
         // generate camera and add to node
@@ -77,16 +88,17 @@ static SCNGeometryPrimitiveType GLTFSCNGeometryPrimitiveTypeForPrimitiveType(GLT
     //    @property (nonatomic, copy) NSString *jointName;
     
     scnNode.simdTransform = node.localTransform;
-    
-    [scnNode addChildNode:childRoot];
+    scnNode.name = node.name;
+
+    [parentNode addChildNode:scnNode];
     
     NSArray<SCNNode *> *meshNodes = [self nodesForGLTFMesh:node.mesh];
     for (SCNNode *meshNode in meshNodes) {
-        [childRoot addChildNode:meshNode];
+        [scnNode addChildNode:meshNode];
     }
-    
+
     for (GLTFNode *child in node.children) {
-        [self recursiveAddNode:child toSCNNode:childRoot];
+        [self recursiveAddNode:child toSCNNode:scnNode];
     }
 }
 
@@ -97,6 +109,7 @@ static SCNGeometryPrimitiveType GLTFSCNGeometryPrimitiveTypeForPrimitiveType(GLT
     
     NSMutableArray *nodes = [NSMutableArray array];
 
+    NSInteger submeshIndex = 0;
     for (GLTFSubmesh *submesh in mesh.submeshes) {
         NSMutableArray *sources = [NSMutableArray array];
         NSMutableArray *elements = [NSMutableArray array];
@@ -113,6 +126,12 @@ static SCNGeometryPrimitiveType GLTFSCNGeometryPrimitiveTypeForPrimitiveType(GLT
             [sources addObject:normalSource];
         }
         
+        SCNGeometrySource *tangentSource = [self geometrySourceWithSemantic:SCNGeometrySourceSemanticTangent
+                                                                   accessor:submesh.accessorsForAttributes[GLTFAttributeSemanticTangent]];
+        if (tangentSource != nil) {
+            [sources addObject:tangentSource];
+        }
+
         SCNGeometrySource *texCoord0Source = [self geometrySourceWithSemantic:SCNGeometrySourceSemanticTexcoord
                                                                      accessor:submesh.accessorsForAttributes[GLTFAttributeSemanticTexCoord0]];
         if (texCoord0Source != nil) {
@@ -121,7 +140,6 @@ static SCNGeometryPrimitiveType GLTFSCNGeometryPrimitiveTypeForPrimitiveType(GLT
         
         // TODO:
         //   SCNGeometrySourceSemanticColor;
-        //   SCNGeometrySourceSemanticTangent
         //   SCNGeometrySourceSemanticBoneWeights
         //   SCNGeometrySourceSemanticBoneIndices
         
@@ -133,7 +151,7 @@ static SCNGeometryPrimitiveType GLTFSCNGeometryPrimitiveTypeForPrimitiveType(GLT
         NSData *indexData = [NSData dataWithBytesNoCopy:indexBuffer.contents + indexBufferView.offset + indexAccessor.offset
                                                  length:indexAccessor.count * bytesPerIndex
                                            freeWhenDone:NO];
-        NSInteger indexCount = indexAccessor.count / bytesPerIndex;
+        NSInteger indexCount = indexAccessor.count;
         NSInteger primitiveCount = indexCount / 3; // TODO: Wrong for anything other than indexed triangles
         SCNGeometryElement *geometryElement = [SCNGeometryElement geometryElementWithData:indexData
                                                                             primitiveType:primitiveType
@@ -143,14 +161,16 @@ static SCNGeometryPrimitiveType GLTFSCNGeometryPrimitiveTypeForPrimitiveType(GLT
         
         SCNGeometry *geometry = [SCNGeometry geometryWithSources:sources elements:elements];
         
+        SCNMaterial *material = [self materialForGLTFMaterial:submesh.material];
+        geometry.materials = @[material];
+        
         SCNNode *node = [SCNNode node];
         node.geometry = geometry;
         
         [nodes addObject:node];
+        ++submeshIndex;
     }
     
-    // TODO: Materials!
-
     return nodes;
 }
 
@@ -169,11 +189,11 @@ static SCNGeometryPrimitiveType GLTFSCNGeometryPrimitiveTypeForPrimitiveType(GLT
     NSInteger dataOffset = 0;
     NSInteger dataStride = accessor.bufferView.stride;
     if (dataStride == 0) {
-        dataStride = GLTFSizeOfComponentTypeWithDimension(accessor.componentType, accessor.dimension);
+        dataStride = bytesPerElement;
     }
     
     NSData *data = [NSData dataWithBytesNoCopy:buffer.contents + bufferView.offset + accessor.offset
-                                        length:accessor.count * bytesPerElement
+                                        length:accessor.count * dataStride
                                   freeWhenDone:NO];
 
     SCNGeometrySource *source = [SCNGeometrySource geometrySourceWithData:data
@@ -185,6 +205,165 @@ static SCNGeometryPrimitiveType GLTFSCNGeometryPrimitiveTypeForPrimitiveType(GLT
                                                                dataOffset:dataOffset
                                                                dataStride:dataStride];
     return source;
+}
+
+- (SCNMaterial *)materialForGLTFMaterial:(GLTFMaterial *)material {
+    if (material == nil) {
+        return nil;
+    }
+    
+    SCNMaterial *scnMaterial = [SCNMaterial material];
+    
+    scnMaterial.name = material.name;
+    
+    scnMaterial.lightingModelName = SCNLightingModelPhysicallyBased;
+    
+    // TODO: Translate from addressing modes to proper wrapping modes
+    
+    scnMaterial.diffuse.contents = (__bridge id)[self cgImageForGLTFImage:material.baseColorTexture.image
+                                                              channelMask:GLTFImageChannelAll];
+    if (scnMaterial.diffuse.contents == nil) {
+        scnMaterial.diffuse.contents = (__bridge id)[self createCGColorForFloat4:material.baseColorFactor];
+    }
+    scnMaterial.diffuse.wrapS = SCNWrapModeRepeat;
+    scnMaterial.diffuse.wrapT = SCNWrapModeRepeat;
+    scnMaterial.diffuse.mappingChannel = material.baseColorTexCoord;
+    
+    scnMaterial.metalness.contents = (__bridge id)[self cgImageForGLTFImage:material.metallicRoughnessTexture.image
+                                                                channelMask:GLTFImageChannelBlue];
+    if (scnMaterial.metalness.contents == nil) {
+        scnMaterial.metalness.contents = @(material.metalnessFactor);
+    }
+    scnMaterial.metalness.wrapS = SCNWrapModeRepeat;
+    scnMaterial.metalness.wrapT = SCNWrapModeRepeat;
+    scnMaterial.metalness.mappingChannel = material.metallicRoughnessTexCoord;
+    
+    scnMaterial.roughness.contents = (__bridge id)[self cgImageForGLTFImage:material.metallicRoughnessTexture.image
+                                                                channelMask:GLTFImageChannelGreen];
+    if (scnMaterial.roughness.contents == nil) {
+        scnMaterial.roughness.contents = @(material.roughnessFactor);
+    }
+    scnMaterial.roughness.wrapS = SCNWrapModeRepeat;
+    scnMaterial.roughness.wrapT = SCNWrapModeRepeat;
+    scnMaterial.roughness.mappingChannel = material.metallicRoughnessTexCoord;
+    
+    scnMaterial.normal.contents = (__bridge id)[self cgImageForGLTFImage:material.normalTexture.image
+                                                             channelMask:GLTFImageChannelAll];
+    scnMaterial.normal.wrapS = SCNWrapModeRepeat;
+    scnMaterial.normal.wrapT = SCNWrapModeRepeat;
+    scnMaterial.normal.mappingChannel = material.normalTexCoord;
+    
+    scnMaterial.ambientOcclusion.contents = (__bridge id)[self cgImageForGLTFImage:material.occlusionTexture.image
+                                                                       channelMask:GLTFImageChannelRed];
+    scnMaterial.ambientOcclusion.wrapS = SCNWrapModeRepeat;
+    scnMaterial.ambientOcclusion.wrapT = SCNWrapModeRepeat;
+    scnMaterial.ambientOcclusion.mappingChannel = material.occlusionTexCoord;
+    
+    scnMaterial.emission.contents = (__bridge id)[self cgImageForGLTFImage:material.emissiveTexture.image
+                                                               channelMask:GLTFImageChannelAll];
+    if (scnMaterial.emission.contents == nil) {
+        scnMaterial.emission.contents = (__bridge id)[self createCGColorForFloat3:material.emissiveFactor];
+    }
+    scnMaterial.emission.wrapS = SCNWrapModeRepeat;
+    scnMaterial.emission.wrapT = SCNWrapModeRepeat;
+    scnMaterial.emission.mappingChannel = material.emissiveTexCoord;
+
+    return scnMaterial;
+}
+
+- (CGImageRef)cgImageForGLTFImage:(GLTFImage *)image channelMask:(GLTFImageChannel)channelMask {
+    if (image == nil) {
+        return nil;
+    }
+    
+    NSString *maskedIdentifier = [NSString stringWithFormat:@"%@/%d", image.identifier.UUIDString, (int)channelMask];
+
+    // Check the cache to see if we already have an exact match for the requested image and channel subset
+    CGImageRef exactCachedImage = (__bridge CGImageRef)self.cgImagesForImagesAndChannels[maskedIdentifier];
+    if (exactCachedImage != nil) {
+        return exactCachedImage;
+    }
+
+    // If we don't have an exact match for the image+channel pair, we may still have the original image cached
+    NSString *unmaskedIdentifier = [NSString stringWithFormat:@"%@/%d", image.identifier.UUIDString, (int)GLTFImageChannelAll];
+    CGImageRef originalImage = (__bridge CGImageRef)self.cgImagesForImagesAndChannels[unmaskedIdentifier];
+
+    if (originalImage == NULL) {
+        // We got unlucky, so we need to load and cache the original
+        if (image.cgImage != nil) {
+            originalImage = image.cgImage;
+            CGImageRetain(originalImage);
+        } else if (image.url != nil) {
+            CGImageSourceRef imageSource = CGImageSourceCreateWithURL((__bridge CFURLRef)image.url, nil);
+            originalImage = CGImageSourceCreateImageAtIndex(imageSource, 0, nil);
+            if (imageSource) {
+                CFRelease(imageSource);
+            }
+        }
+        
+        self.cgImagesForImagesAndChannels[unmaskedIdentifier] = (__bridge id)originalImage;
+        CGImageRelease(originalImage);
+    }
+    
+    // Now that we have the original, we may need to extract the requisite channel and cache the result
+    if (channelMask != GLTFImageChannelAll) {
+        CGImageRef extractedImage = [self createCGImageByExtractingChannel:(int)channelMask fromCGImage:originalImage];
+        self.cgImagesForImagesAndChannels[maskedIdentifier] = (__bridge id)extractedImage;
+        CGImageRelease(extractedImage);
+        return extractedImage;
+    }
+    
+    return originalImage;
+}
+
+- (CGImageRef)createCGImageByExtractingChannel:(NSInteger)channelIndex fromCGImage:(CGImageRef)sourceImage {
+    if (sourceImage == NULL) {
+        return NULL;
+    }
+    
+    size_t width = CGImageGetWidth(sourceImage);
+    size_t height = CGImageGetHeight(sourceImage);
+    size_t bpc = 8;
+    size_t Bpr = width * 4;
+
+    uint8_t *pixels = malloc(Bpr * height);
+    
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateWithName(kCGColorSpaceSRGB);
+    CGContextRef context = CGBitmapContextCreate(pixels, width, height, bpc, Bpr, colorSpace, kCGImageAlphaPremultipliedLast);
+    CGContextDrawImage(context, CGRectMake(0, 0, width, height), sourceImage);
+    
+    for (int i = 0; i < width * height; ++i) {
+        uint8_t components[4] = { pixels[i * 4 + 0], pixels[i * 4 + 1], pixels[i * 4 + 2], pixels[i * 4 + 3] }; // RGBA
+        pixels[i] = components[channelIndex];
+    }
+    
+    CGColorSpaceRef monoColorSpace = CGColorSpaceCreateWithName(kCGColorSpaceGenericGrayGamma2_2);
+    CGContextRef monoContext = CGBitmapContextCreate(pixels, width, height, bpc, width, monoColorSpace, kCGImageAlphaNone);
+
+    CGImageRef channelImage = CGBitmapContextCreateImage(monoContext);
+
+    CGContextRelease(monoContext);
+    CGColorSpaceRelease(colorSpace);
+    CGContextRelease(context);
+    free(pixels);
+    
+    return channelImage;
+}
+
+- (CGColorRef)createCGColorForFloat4:(simd_float4)v {
+    CGFloat components[] = { v.x, v.y, v.z, v.w, 0 };
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateWithName(kCGColorSpaceSRGB);
+    CGColorRef color = CGColorCreate(colorSpace, &components[0]);
+    CGColorSpaceRelease(colorSpace);
+    return color;
+}
+
+- (CGColorRef)createCGColorForFloat3:(simd_float3)v {
+    CGFloat components[] = { v.x, v.y, v.z, 1, 0 };
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateWithName(kCGColorSpaceSRGB);
+    CGColorRef color = CGColorCreate(colorSpace, &components[0]);
+    CGColorSpaceRelease(colorSpace);
+    return color;
 }
 
 @end
